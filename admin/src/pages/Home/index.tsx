3add4d718c29685028305b1d34ec345b21c7b1e1
Nguyen Trung Duc <huyga612@gmail.com>
2023-08-22 12:02:22 +0700

    base admin and client

---
+++ admin/src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Home({}: Props) {
+  return (
+    <div className='text-red-500'>index1</div>
+  )
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import store, { persistor } from './app/store.tsx';
+import { PersistGate } from 'redux-persist/integration/react'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </BrowserRouter >
 )
