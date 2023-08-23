2f077650cedf6c308f031a27580770100ecbb6b7
DT Hang <dongthihang7903@gmail.com>
2023-08-23 17:51:29 +0700

    giao diện login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Login from "./pages/Auth/login"
 
 
 function App() {
- 
+
 
   return (
     <>
       <Routes>
-        <Route path ='/'> 
-          <Route index element = {<Home/>}/>
+        <Route path='/'>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
         </Route>
       </Routes>
     </>
