7f9fb73ad46d1d26099dc621ee03fce5c9d3d7c2
DT Hang <dongthihang7903@gmail.com>
2023-08-25 13:37:54 +0700

    giao dien login admin

---
+++ admin/src/pages/Auth/register.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function RegisterAdmin({ }: Props) {
+    return (
+        <div className='text-red-500'>registeradmin</div>
+    )
+}
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
+
         </Route>
       </Routes>
     </>
