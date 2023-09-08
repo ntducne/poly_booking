899f9d5cb0fe8217bf6a285e9f900805cb90e3bd
Nguyen Trung Duc <huyga612@gmail.com>
2023-09-08 21:29:15 +0700

    pull and fix conflick

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,13 +6,10 @@ import Home from "./pages/Home"
 import Login from "./pages/Auth/login"
 import Register from "./pages/Auth/register"
 import Rooms from "./pages/Rooms"
-<<<<<<< HEAD
 import { Profiler } from "react"
 import Profile from "./pages/Auth/profile"
 import Checkout from "./pages/Checkout"
-=======
 import RoomBooked from "./pages/RoomBooked"
->>>>>>> 27b67e504cc0dd0518a6d0ae0c9bc94fb61a3e97
 
 
 function App() {
@@ -25,23 +22,20 @@ function App() {
         <Route path='/' element={<LayoutClient />}>
           <Route index element={<Home />} />
           <Route path='rooms' element={<Rooms />} />
-<<<<<<< HEAD
           <Route path="detail" element={<Detial />} />
           <Route path="contact" element={<Contact />} />
           <Route path="profile" element={<Profile />} />
           <Route path="checkout" element={<Checkout />} />
-=======
           <Route path='user'>
             <Route path='roomBooked' element={<RoomBooked />} />
           </Route>
->>>>>>> 27b67e504cc0dd0518a6d0ae0c9bc94fb61a3e97
         </Route>
 
         <Route path='/auth'>
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
-         
-          
+
+
         </Route>
       </Routes>
     </>
