dca6b3232892f599f6fd40a5b98638e155038532
Nguyen Trung Duc <huyga612@gmail.com>
2023-10-02 15:24:59 +0700

    fix button search

--- client/src/components/BookForm/index.tsx
+++ client/src/components/BookForm/index.tsx
@@ -87,7 +87,7 @@ export default function BookForm({ }: Props) {
                         </div>
                         <div className=''>
                             {/* <Form.Item className='p-0 m-0'> */}
-                            <button className='bg-primary  p lg:h-full active:bg-black px-5 w-full justify-center md:h-[65px] flex items-center rounded-none'>
+                            <button className='bg-primary  p lg:h-full h-[40px] active:bg-black px-5 w-full justify-center md:h-[65px] flex items-center rounded-none'>
                                 <SearchOutlined className='lg:px-5 text-[18px] lg:text-[20px] text-secondary' />
                             </button>
                             {/* </Form.Item> */}
