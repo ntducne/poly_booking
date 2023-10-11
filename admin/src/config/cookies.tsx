adfdf7ff77779f4f5933649a00eb03d471a10a91
Nguyễn Đức <nguyenduc10603@gmail.com>
2023-10-11 20:49:38 +0700

    fiX

---
+++ admin/src/config/cookies.tsx
@@ -0,0 +1,26 @@
+import { decode, encode } from 'js-base64';
+import { parseCookies, setCookie, destroyCookie } from 'nookies';
+export const cookies = () => {
+    const cookies = parseCookies()
+    const Get = (name: string) => {
+        if (cookies[name]) {
+            return decode(cookies[name])
+        }
+        return null
+    }
+    const Set = (name: string, values: any, experied: any) => {
+        setCookie(null, name , encode(values), { 
+            maxAge: experied,
+            path: '/', 
+            secure: true ,
+        })
+    }
+    const Delete = (name: string) => {
+        destroyCookie(null, name)
+    }
+    return {
+        Get,
+        Set,
+        Delete
+    }
+}
