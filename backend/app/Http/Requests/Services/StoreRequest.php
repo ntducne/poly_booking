d230b9cd2683a045c9e3cc3b98f3c6fe5d5058eb
dungtuan1902 <phamtuandung923@gmail.com>
2023-10-17 00:47:07 +0700

    cập nhật StoreRequest + chức năng đặt phòng có thông tin phòng

--- backend/app/Http/Requests/Services/StoreRequest.php
+++ backend/app/Http/Requests/Services/StoreRequest.php
@@ -2,26 +2,28 @@
 
 namespace App\Http\Requests\Services;
 
+use App\Models\Branch;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreRequest extends FormRequest
 {
     public function rules()
     {
         return [
-           'service_name'=>['required'],
-           'price'=>['required','numeric'],
-           'description'=>['required'],
-           'branch_id'=>['required']
+            'service_name' => ['required'],
+            'price' => ['required', 'numeric'],
+            'description' => ['required'],
+            'branch_id' => ['required', 'exists:App\Models\Branch,_id']
         ];
     }
     public function attributes()
     {
         return [
-            'service_name'   => 'Tên dịch vụ',
-            'price'      => 'Giá',
-            'description'     => 'Mô tả',
-            'branch_id'     => 'ID chi nhánh',
+            'service_name' => 'Tên dịch vụ',
+            'price' => 'Giá',
+            'description' => 'Mô tả',
+            'branch_id' => 'ID chi nhánh',
         ];
     }
 }
--- backend/routes/api/client.php
+++ backend/routes/api/client.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ServicesController;
 use Illuminate\Support\Facades\Route;
 Route::prefix('room')->group(function (){
     Route::get('/', [ClientController::class, 'rooms']);
@@ -8,3 +9,4 @@ Route::prefix('room')->group(function (){
     Route::post('/search', [ClientController::class, 'search']);
     Route::post('/booking', [ClientController::class, 'booking']);
 });
+Route::resource('services', ServicesController::class)->except(['create','edit']);
