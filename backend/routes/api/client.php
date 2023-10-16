e4f15873bd551305ac67fe783a56b093e466c978
dungtuan1902 <phamtuandung923@gmail.com>
2023-10-17 00:51:28 +0700

    update

--- backend/routes/api/client.php
+++ backend/routes/api/client.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\ServicesController;
 use Illuminate\Support\Facades\Route;
 Route::prefix('room')->group(function (){
     Route::get('/', [ClientController::class, 'rooms']);
@@ -9,4 +8,3 @@ Route::prefix('room')->group(function (){
     Route::post('/search', [ClientController::class, 'search']);
     Route::post('/booking', [ClientController::class, 'booking']);
 });
-Route::resource('services', ServicesController::class)->except(['create','edit']);
