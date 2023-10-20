6dd76f79150c05384886e2bbaedb4abb78959133
Nguyễn Đức <nguyenduc10603@gmail.com>
2023-10-20 17:16:20 +0700

    up

--- backend/app/Http/Resources/BillingResource.php
+++ backend/app/Http/Resources/BillingResource.php
@@ -2,31 +2,22 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Booking;
+use App\Models\Branch;
 use App\Models\Services;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class BillingResource extends JsonResource
 {
-    private function getService(){
-        $arrService = [];
-        foreach ($this->sevice as $item){
-            $arrService[] = [
-                'name' => Services::find($item->service_name),
-                'price' => Services::find($item->price),
-            ];
-
-        }
-    }
-
     public function toArray($request)
     {
         return [
-            'booking'=> new BookingResource($this->booking_id),
+            'booking'=> new BookingResource(Booking::find($this->booking_id)),
             'services'=>$this->services,
             'total'=>$this->total,
             'payment_method'=> $this->payment_method,
             'payment_date'=>$this->payment_date,
-            'branch'=> new BranchResource($this->branch_id),
+            'branch'=> new BranchResource(Branch::find($this->branch_id)),
             'status'=> $this->status
         ];
     }
