2d3cc97e42675521fc8fe2499f8532ed32a8d21b
muinguyen03 <muinguyen03@gmail.com>
2023-11-08 21:47:54 +0700

    update code

--- backend/routes/api/client.php
+++ backend/routes/api/client.php
@@ -4,19 +4,13 @@ use App\Http\Controllers\Admin\BillingController;
 use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
-
-Route::prefix('room')->group(function (){
-=======
 Route::fallback(function () {
     return response()->json([
         'message' => 'Page Not Found'
     ], 404);
 });
-Route::get('type', [\App\Http\Controllers\Admin\RoomTypeController::class,'index']);
-Route::get('branch', [\App\Http\Controllers\Admin\BranchController::class,'index']);
+Route::get('branch',[ClientController::class,'branch']);
 Route::prefix('room')->group(function () {
->>>>>>> 633473358f6f515991122fda88ef6d201a2f59ab
     Route::get('/', [ClientController::class, 'rooms']);
     Route::get('/type', [ClientController::class, 'roomType']);
     Route::get('/search', [ClientController::class, 'search']);
