68dbe696c665dc48f9b97547b1ff09b9a534e700
Nguyễn Đức <nguyenduc10603@gmail.com>
2023-11-10 08:20:49 +0700

    pull

--- client/src/pages/demo.tsx
+++ client/src/pages/demo.tsx
@@ -1,11 +1,4 @@
-export default function Demo() { 
-    fetch('http://localhost:5000/api/v1/users',{
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`
-        }
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
+export default function Demo() {
     return (
         <>aHIHI</>
     )
