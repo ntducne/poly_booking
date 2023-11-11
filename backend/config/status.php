794be5b7f21879fb354607526c3e50ae0a4c7158
muinguyen03 <muinguyen03@gmail.com>
2023-11-11 12:57:14 +0700

    create config/status.php

---
+++ backend/config/status.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+        [
+            'status' => 'Chờ nhận phòng',
+        ],
+        [
+            'status' => 'Hủy Đặt phòng',
+        ],
+        [
+            'status' => 'Đã nhận phòng',
+        ],
+        [
+            'status' => 'Đã trả phòng',
+        ],
+        [
+            'status' => 'Đã thanh toán',
+        ],
+        [
+            'status' => 'Hủy Thanh Toán',
+        ],
+        [
+            'status' => 'Thanh toán lỗi',
+        ],
+        [
+            'status' => 'Hoạt động',
+        ],
+        [
+            'status' => 'Tạm đóng',
+        ],
+        [
+            'status' =>  'Cấm'
+        ],
+];
