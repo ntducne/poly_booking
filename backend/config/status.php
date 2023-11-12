ccd3f453e326b08a275948d547f186ddfc08a327
dungtuan1902 <phamtuandung923@gmail.com>
2023-11-12 12:34:04 +0700

    cập nhật status của chức năng đặt phòng và tìm kiếm của client và admin

--- backend/config/status.php
+++ backend/config/status.php
@@ -1,44 +1,48 @@
 <?php
 
 return [
-        [
-            'id'     => 1,
-            'status' => 'Chờ nhận phòng',
-        ],
-        [
-            'id'     => 2,
-            'status' => 'Hủy Đặt phòng',
-        ],
-        [
-            'id'     => 3,
-            'status' => 'Đã nhận phòng',
-        ],
-        [
-            'id'     => 4,
-            'status' => 'Đã trả phòng',
-        ],
-        [
-            'id'     => 5,
-            'status' => 'Đã thanh toán',
-        ],
-        [
-            'id'     => 6,
-            'status' => 'Hủy Thanh Toán',
-        ],
-        [
-            'id'     => 7,
-            'status' => 'Thanh toán lỗi',
-        ],
-        [
-            'id'     => 8,
-            'status' => 'Hoạt động',
-        ],
-        [
-            'id'     => 9,
-            'status' => 'Tạm đóng',
-        ],
-        [
-            'id'     => 10,
-            'status' =>  'Cấm'
-        ],
+    [
+        "id" => 0,
+        'status' => 'Đã đặt '
+    ],
+    [
+        'id' => 1,
+        'status' => 'Chờ nhận phòng',
+    ],
+    [
+        'id' => 2,
+        'status' => 'Hủy Đặt phòng',
+    ],
+    [
+        'id' => 3,
+        'status' => 'Đã nhận phòng',
+    ],
+    [
+        'id' => 4,
+        'status' => 'Đã trả phòng',
+    ],
+    [
+        'id' => 5,
+        'status' => 'Đã thanh toán',
+    ],
+    [
+        'id' => 6,
+        'status' => 'Hủy Thanh Toán',
+    ],
+    [
+        'id' => 7,
+        'status' => 'Thanh toán lỗi',
+    ],
+    [
+        'id' => 8,
+        'status' => 'Hoạt động',
+    ],
+    [
+        'id' => 9,
+        'status' => 'Tạm đóng',
+    ],
+    [
+        'id' => 10,
+        'status' => 'Cấm'
+    ],
 ];
