d1b4635c24704c07445749964fb6d6d95b34eca6
Nguyễn Đức <nguyenduc10603@gmail.com>
2023-11-17 22:16:06 +0700

    up

--- backend/app/Models/Booking.php
+++ backend/app/Models/Booking.php
@@ -24,7 +24,7 @@ class Booking extends Eloquent
         'time',
     ];
     protected $attributes = [
-        'status'=> false,
+        'status' => false,
         'deleted_at' => null,
         'people' => null
     ];
