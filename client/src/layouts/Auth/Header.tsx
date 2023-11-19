0b141caa1df7afa27e4e6c2bfa681231db26e569
DT Hang <dongthihang7903@gmail.com>
2023-11-19 12:41:11 +0700

    sửa lại giao diện login register

--- client/src/layouts/Auth/Header.tsx
+++ client/src/layouts/Auth/Header.tsx
@@ -10,7 +10,7 @@ export default function HeaderAuth() {
         <div className={`bg-white shadow-lg py-[3px] px-[50px] top-0 z-40 w-full transition-all duration-300 ${header ? 'relative' : 'absolute'}`}>
             <div className="container mx-auto lg:flex-row lg:justify-between lg:gap-y-0 flex justify-between ">
                 <div className={`flex gap-5 items-center ${header ? 'relative' : 'absolute'}`}>
-                    <Link to=''>
+                    <Link to='/'>
                         <img className='w-[90px]' src={"https://res.cloudinary.com/dteefej4w/image/upload/v1696338661/logo_30_zwmslg.png"} />
                     </Link>
                     <div className={`${header ? "text-dark py-6" : "text-white  py-4"} lg:flex gap-2 lg:gap-x-8 md:tracking-[3px] tracking-[1px] md:text-[15px] items-center hidden`}>
