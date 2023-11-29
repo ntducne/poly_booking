fed1567f19a4ed8e64d2c06b689447d551415193
Nguyễn Đức <nguyenduc10603@gmail.com>
2023-11-29 17:34:16 +0700

    update stat

--- backend/app/Modules/Dashboard/Controllers/DashboardController.php
+++ backend/app/Modules/Dashboard/Controllers/DashboardController.php
@@ -5,29 +5,16 @@ namespace App\Modules\Dashboard\Controllers;
 use App\Http\Controllers\Controller;
 use App\Repositories\StatsRepository;
 use Illuminate\Http\Request;
-
 class DashboardController extends Controller 
 {
-    // public function __construct(StatsRepository $statsRepository)
-    // {
-    //     $this->statsRepository = new StatsRepository();
-    // }
+    public function statistical(Request $request){
+        $this->statsRepository = new StatsRepository();
+        if($request->module == 'revenue'){
+            return $this->statsRepository->revenue($request);
+        } 
+        if($request->module == 'room'){
 
-    public function revenue_statistical(Request $request){
-        $statsRepository = new StatsRepository();
-        return $statsRepository->revenue($request);
-        
-        // dd(1);
-        // if($request->module == 'revenue'){
-        //     return $this->statsRepository->revenue($request);
-        // } 
-        // if($request->module == 'room'){
-
-        //     return $this->statsRepository->room($request);
-        // }
-    }
-
-    public function room(Request $request) {
-        // return $this->statsRepository->room($request);
+            return $this->statsRepository->room($request);
+        }
     }
 }
