22f8a936d399a8291c397f6a2386790761754c49
root <nguyenduc10603@gmail.com>
2023-12-05 23:06:30 +0700

    pull

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -14,20 +14,6 @@ Route::fallback(function () {
     ], 404);
 });
 
-<<<<<<< HEAD
-// Route::middleware(CheckReferer::class)->group(function () {
-    // Route::group(['middleware' => 'throttle:3,5'], function () {
-        Route::post('login', [AuthController::class, 'login']);
-    // });
-    // Route::middleware(CheckType::class)->group(function () {
-        Route::post('register', [AuthController::class, 'register']);
-        Route::group(['middleware' => 'throttle:3,5'], function () {
-            Route::post('reset-password', [ForgotPasswordController::class, 'sendMail']);
-            Route::get('reset-password/{token}', [ForgotPasswordController::class, 'checkToken']);
-            Route::put('reset-password', [ForgotPasswordController::class, 'reset']);
-        });
-    // });
-=======
 Route::middleware(CheckReferer::class)->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::middleware(CheckType::class)->group(function () {
@@ -36,7 +22,6 @@ Route::middleware(CheckReferer::class)->group(function () {
         Route::get('reset-password/{token}', [ForgotPasswordController::class, 'checkToken']);
         Route::put('reset-password', [ForgotPasswordController::class, 'reset']);
     });
->>>>>>> 4666b403f3741bd108b63853aaa1cc234ed184c7
     Route::get('/login/{provider}', [LoginSocicalController::class, 'redirect']);
     Route::get('/callback/{provider}', [LoginSocicalController::class, 'callback']);
-// });
+});
