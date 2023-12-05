203d1dfb14f212cd5ced2740310204cc02beed33
Nguyễn Đức <nguyenduc10603@gmail.com>
2023-12-05 12:49:20 +0700

    fix event billing

--- backend/app/Http/Middleware/Authenticate.php
+++ backend/app/Http/Middleware/Authenticate.php
@@ -12,7 +12,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-//        return $request->expectsJson() ? null : route('login');
-        return $request->expectsJson() ? null : 'unauthorized';
+       return $request->expectsJson() ? null : route('unauthorized');
+        // return $request->expectsJson() ? null : 'unauthorized';
     }
 }
