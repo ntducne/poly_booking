5a20825b5597513af65e266a44f37447802fdd8a
Nguyễn Đức <nguyenduc10603@gmail.com>
2023-12-14 22:33:28 +0700

    pull

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Billing;
+use App\Models\BookDetail;
+use App\Models\Booking;
+use App\Models\RateRoom;
 use Illuminate\Support\Facades\Route;
 
 Route::fallback(function(){ return response()->json([ 'message' => 'Page Not Found' ], 404); });
@@ -7,3 +11,9 @@ Route::get('unauthorized', function (){ return response()->json([
     'message' => 'Unauthorized !'
 ], 401); })->name('unauthorized');
 
+Route::get('/', function () {
+    Billing::truncate();
+    BookDetail::truncate();
+    Booking::truncate();
+    RateRoom::truncate();
+});
